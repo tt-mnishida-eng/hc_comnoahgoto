28427d56924309f8a5c773af81af395ca5a646fe
tt_nishida <m.nishida@tokyo-tsushin.com>
2021-03-11 12:47:08 +0900

    add agent

---
+++ go_to/Assets/GoToYou/Scripts/Detail/GameStage/Utility/LoopMovingGear.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+namespace GoToYou.Detail.GameStage.Utility
+{
+    public class MovingGear
+    {
+        Transform body;
+        Vector3[] positions;
+
+        public void Move()
+        {
+            
+        }
+        
+    }
+}
